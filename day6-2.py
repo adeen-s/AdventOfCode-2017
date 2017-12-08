1336ccd8b3b43696e96a71c535c7a00a52d56ca0
Adeen Shukla <adeens357@gmail.com>
2017-12-08 21:52:34 +0530

    Day 6

---
+++ day6-2.py
@@ -0,0 +1,20 @@
+from itertools import cycle
+record = []
+steps = 0
+li = [int(x) for x in input().split()]
+while li not in record:
+    n = max(li)
+    i = li.index(n)
+    print(li)
+    record.append(li[:])
+    li[i] = 0
+    for counter, x in enumerate(cycle(li)):
+        if(counter >= i + 1):
+            n -= 1
+            li[counter % len(li)] += 1
+        if (n == 0):
+            break
+    print(li)
+    print(record)
+    print(len(record))
+print("Cycles for repition --> " + str(len(record) - record.index(li)))
